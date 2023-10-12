19f2d36f98c902118098ff79763a7ffd31f52077
JuliaS0r <Yulyasor0501@yandex.ru>
2023-10-12 21:17:07 +0300

    проверьте дз 2

--- src/task2.java
+++ src/task2.java
@@ -1,8 +1,11 @@
-public class Main {
+public class task2 {
     public static void main(String[] args) {
-      var dog = 8.0;
-      var cat = 3.6;
-      var paper = 763789;
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+            dog = dog + 4;
+            cat = cat + 4;
+            paper = paper + 4;
         System.out.println("dog = " + dog);
         System.out.println("cat = " + cat);
         System.out.println("paper = " + paper);
